38ce603203089b447c50933d9472d490554afffa
zack <yeo_zh@hotmail.com>
2022-10-01 10:36:16 +0800

    Initial Quiz Game Commit

---
+++ app/src/main/java/com/znotagain/quizgame/ForgotPasswordActivity.kt
@@ -0,0 +1,36 @@
+package com.znotagain.quizgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.znotagain.quizgame.databinding.ActivityForgotPasswordBinding
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    lateinit var forgotBinding: ActivityForgotPasswordBinding
+
+    val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        forgotBinding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        val view = forgotBinding.root
+        setContentView(view)
+
+        forgotBinding.buttonReset.setOnClickListener {
+            val userEmail = forgotBinding.editTextForgotEmail.text.toString()
+            auth.sendPasswordResetEmail(userEmail).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Password reset mail sent to your email address", Toast.LENGTH_SHORT).show()
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, task.exception?.localizedMessage, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+}
